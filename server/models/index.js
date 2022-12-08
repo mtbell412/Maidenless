b35db9d8dc43fdcf760c8225737e15e37df8b98a
Keshon Royster <k.royster512@gmail.com>
2022-12-08 11:36:10 -0600

    filled in index.js for models/schemas

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const AttributePoints = require('./AttributePoints');
+const Character = require('./Character');
+const Equipment = require('./Equipment');
+const User = require('./User');
+
+module.exports = {AttributePoints, Character, Equipment, User};
