2b6e370dc33bb960fe3264fbd76d4518aa5dbc3a
Zach L <ztak78@gmail.com>
2022-12-08 10:58:46 -0600

    added in base  queries and mutations on client side utils folder

---
+++ client/src/utils/queries.js
@@ -0,0 +1,126 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_USERS = gql`
+    query users{
+        users{
+            _id
+            username
+            character{
+                _id
+                name
+                class
+                created at
+            }
+        }
+    }
+`
+
+export const QUERY_SINGLE_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      characters {
+        _id
+        name
+        class
+        createdAt
+      }
+    }
+  }
+`;
+
+export const QUERY_USER_CHARACTER = gql`
+  query getCharacters {
+    characters {
+        _id
+        name
+        class
+        createdAt
+    }
+  }
+`;
+
+export const QUERY_SINGLE_CHARACTER = gql`
+  query getUserCharacters($userId: ID!) {
+    character(characterId: $characterId) {
+      _id
+      name
+      class
+      weapons {
+        name
+        image
+        category
+        weight
+        attack
+        requiredAttributes
+        scalesWith
+      }
+      shield{
+        name
+        image
+        category
+        weight
+        defense
+        requiredAttributes
+        scalesWith
+      }
+      helmet {
+        name
+        image
+        type
+        weight
+        dmgNegation
+        resistance
+      }
+      chestArmor {
+        name
+        image
+        type
+        weight
+        dmgNegation
+        resistance
+      }
+      arms {
+        name
+        image
+        type
+        weight
+        dmgNegation
+        resistance
+      }
+      leg {
+        name
+        image
+        type
+        weight
+        dmgNegation
+        resistance
+      }
+      talismans {
+        name
+        image
+        effects
+      }
+      createdAt
+     
+    }
+  }
+`;
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      character {
+        _id
+        name
+        class
+        createdAt
+      }
+    }
+  }
+`;
